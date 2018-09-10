6bb617b921fe3448e17c20ce1b6774c2801bef38
oliver <myungsekyo@gmail.com>
2018-09-10 17:08:33 +0900

    Fixed #29725 -- Fixed inefficient sql generating when calling count() on a many to many relation
    Fixed #29725 -- Fixed inefficient sql generating when calling count() on a many to many relation
    
    
    Fixed #29725 -- Fixed inefficient sql generating when calling count() on a many to many relation

--- django/db/models/fields/related_descriptors.py
+++ django/db/models/fields/related_descriptors.py
@@ -912,6 +912,18 @@ def create_forward_many_to_many_manager(superclass, rel, reverse):
                 False,
             )
 
+        def count(self):
+            # If the through's target field's foreign integrity is enforced
+            # the COUNT can be performed against the through table instead of
+            # INNER JOIN'ing the target table.
+            if self.target_field.db_constraint:
+                hints = {'instance': self.instance}
+                manager = self.through._base_manager.db_manager(using=self._db, hints=hints)
+                return manager.filter(
+                    **{self.source_field_name: self.instance.pk}
+                ).count()
+            return super().count()
+
         def add(self, *objs):
             if not rel.through._meta.auto_created:
                 opts = self.through._meta
